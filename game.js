d7cfe4a00255dd3011848e074f652db2d9e9b646
Jingcheng Wang <jingwang49@gmail.com>
2024-02-12 17:27:27 +0800

    snake basic

--- game.js
+++ game.js
@@ -1,26 +1,27 @@
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js'
+
 let lastRenderTime = 0
-const SNAKE_SPEED = 1
+const gameBoard = document.getElementById('game-board')
+
 
 function main(currentTime) {
-    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
-    if (secondsSinceLastRender < 1 / SNAKE_SPEED) {
-        return
-    }
     window.requestAnimationFrame(main)
-    console.log('Render')
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return 
+    
     lastRenderTime = currentTime
 
     update()
     draw()
 }
-    
 
 window.requestAnimationFrame(main)
 
 function update() {
-
+    updateSnake()
 }
 
 function draw() {
-    
+    gameBoard.innerHTML = ''
+    drawSnake(gameBoard)
 }
